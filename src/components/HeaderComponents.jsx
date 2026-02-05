a7f175665c19f083bdbab05e3198d8bf99e3dbf4
Kashish <kashishrathod508@gmail.com>
2026-02-05 10:27:40 +0530

    learning26

---
+++ src/components/HeaderComponents.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/css/header.css"
+
+export const HeaderComponents = () => {
+  return (
+    <div className ="header" >
+
+
+        <h1>HeaderComponents</h1>
+        </div>
+  )
+}
---
+++ src/components/Homecomponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Homecomponents = () => {
+  return (
+    <div style={{textAlign:'center'}}>
+        <h1>Homecomponents</h1>
+        </div>
+  )
+}
